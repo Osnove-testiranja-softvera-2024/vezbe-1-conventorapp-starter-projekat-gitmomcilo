a4fb89e4ae4788125a58d1738981e4ea41d1a59c
gitmomcilo <maravicmomcilo@gmail.com>
2024-03-06 10:54:50 +0100

    Uradjen zadatak, vezbe2

--- OTS2023_ConventorApp/Form1.Designer.cs
+++ OTS2023_ConventorApp/Form1.Designer.cs
@@ -28,13 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.radMass = new System.Windows.Forms.RadioButton();
+            this.radLength = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(339, 331);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // radMass
+            // 
+            this.radMass.AutoSize = true;
+            this.radMass.Location = new System.Drawing.Point(97, 42);
+            this.radMass.Name = "radMass";
+            this.radMass.Size = new System.Drawing.Size(50, 17);
+            this.radMass.TabIndex = 1;
+            this.radMass.TabStop = true;
+            this.radMass.Text = "Mass";
+            this.radMass.UseVisualStyleBackColor = true;
+            this.radMass.CheckedChanged += new System.EventHandler(this.radMass_CheckedChanged);
+            // 
+            // radLength
+            // 
+            this.radLength.AutoSize = true;
+            this.radLength.Location = new System.Drawing.Point(97, 65);
+            this.radLength.Name = "radLength";
+            this.radLength.Size = new System.Drawing.Size(58, 17);
+            this.radLength.TabIndex = 2;
+            this.radLength.TabStop = true;
+            this.radLength.Text = "Length";
+            this.radLength.UseVisualStyleBackColor = true;
+            this.radLength.CheckedChanged += new System.EventHandler(this.radLength_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Imperial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Metric";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 134);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(189, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radLength);
+            this.Controls.Add(this.radMass);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.RadioButton radMass;
+        private System.Windows.Forms.RadioButton radLength;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- OTS2023_ConventorApp/Form1.cs
+++ OTS2023_ConventorApp/Form1.cs
@@ -16,5 +16,43 @@ namespace OTS2023_ConventorApp
         {
             InitializeComponent();
         }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (radMass.Checked == true) 
+            {
+            
+
+                double masa = int.Parse(textBox1.Text) / 2.2046;
+                textBox2.Text = masa.ToString();    
+
+            }
+            if (radLength.Checked == true)
+            {
+   
+
+                double duzina = int.Parse(textBox1.Text) * 0.3048;
+                textBox2.Text = duzina.ToString(); 
+            }
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radMass_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "pounds";
+            label2.Text = "kilograms";
+        }
+
+        private void radLength_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "feet";
+            label2.Text = "meters";
+        }
     }
-}
+    }
+
